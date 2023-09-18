80a4233b8e9d52465ef99472e0bfa42523765591
fenidp <114385571+fenidp@users.noreply.github.com>
2023-09-18 08:35:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kotlindasar/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kotlindasar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun main(){
+        println("Selamat datang")
+    }
+}
